6923e50f0757d7a10a2acb7b990aaf9a9e4e674c
Raja Shekar <irajashekarb@gmail.com>
2019-10-05 05:28:27 +0530

    Lambda expr program

--- Example programs/src/programs/LambdaExp.scala
+++ Example programs/src/programs/LambdaExp.scala
@@ -1,11 +1,23 @@
 package programs
 
 object LambdaExp {
-  def main(args: Array[String]) = {
+  def main(args: Array[String]): Unit = {
+
+    // Function that uses variable names in lambda expression
     val square = ( x: Double )=> x * x
+
+    // Function that uses underscore notation in lambda expression
     val square_with_underscore: Double => Double = _ * 5
+
+    // Function that takes two arguments and returns boolean for comparision
+    val lt: (Double, Double) => Boolean = _ < _
+
+    // Assigning variable x to some random value
     val x:Int = 5
+
+    // printing out results
     println(s"Square of $x is ${square(x).toInt}")
     println(s"Square of $x is ${square_with_underscore(x).toInt}")
+    println(lt(5, 6))
   }
 }
