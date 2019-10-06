6ee78130056213889b0c64d8fba09704711b8dab
Raja Shekar <irajashekarb@gmail.com>
2019-10-06 19:44:56 +0530

    Update ObjectAndClasses.scala

--- Example programs/src/programs/ObjectAndClasses.scala
+++ Example programs/src/programs/ObjectAndClasses.scala
@@ -3,7 +3,9 @@ package programs
 object ObjectAndClasses {
   def main(args: Array[String]): Unit = {
     val students = List(new Student("Jane", "Doe"), new Student("John", "Allen"))
-    for(s <- students) printStudent(s)
+    for(s <- students) {
+      printStudent(s)
+    }
   }
   def printStudent(s: Student): Unit = {
     println(s.firstName + " " + s.lastName)
