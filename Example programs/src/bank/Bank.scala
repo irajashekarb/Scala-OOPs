8e07642ece8f648dc5d6faa1c28963d31433f677
Raja Shekar <irajashekarb@gmail.com>
2019-10-07 17:08:30 +0530

    Update BankMain.scala

--- Example programs/src/bank/Bank.scala
+++ Example programs/src/bank/Bank.scala
@@ -1,15 +1,43 @@
 package bank
 
-class Bank (private val customer: List[Customer], private val account: List[Account]) {
-//  def addCustomer(fname: String, lname: String): Unit = {
-//
-//  }
-//
-//  def openAccount(c: Customer): Account = {
-//
-//  }
-//
-//  def closeAccount(a: Account): Boolean = {
-//
-//  }
+class Bank {
+  private var _customers: List[Customer] = Nil
+  private var _accounts: List[Account] = Nil
+  private var nextCustomerNumber = 0
+  private var nextAccountNumber = 0
+
+  def addCustomer(fname: String, lname: String, address: Address): Unit = {
+    nextCustomerNumber += 7
+    _customers ::= new Customer(fname, lname, nextCustomerNumber.toString, address)
+  }
+
+  def openAccount(c: Customer): Account = {
+    nextAccountNumber += 13
+    val account = new Account(c, nextCustomerNumber.toString)
+    _accounts ::= account
+    account
+  }
+
+  def closeAccount(a: Account): Boolean = {
+    if(_accounts.contains(a)) {
+      _accounts = _accounts.filter(_ != a)
+      if(!a.customer.removeAccount(a.id)) {
+        println("Account is not part of customer.")
+      }
+      true
+    } else false
+
+  }
+
+  def findCustomer(fname: String, lname: String): Option[Customer] = {
+    _customers.find(c => c.firstName == fname && c.lastName == lname)
+  }
+
+  def findCustomer(id: String): Option[Customer] = {
+    _customers.find(_.id == id)
+  }
+
+  def findAccount(id: String): Option[Account] = {
+    _accounts.find(_.id == id)
+  }
 }
--- Example programs/src/bank/BankMain.scala
+++ Example programs/src/bank/BankMain.scala
@@ -1,5 +1,33 @@
 package bank
 
-object BankMain {
+import scala.io.StdIn._
 
+object BankMain {
+  def main(args: Array[String]): Unit = {
+    val bank = new Bank
+    var option = 0
+    var customer: Option[Customer] = None
+    var account: Option[Customer] = None
+
+
+    while(option != 10) {
+      println(menu)
+      option = readInt()
+      option match {
+        case 1 =>
+        case 2 =>
+        case 3 =>
+        case 4 =>
+        case 5 =>
+        case 6 =>
+        case 7 =>
+        case 8 =>
+        case 9 =>
+        case 10 =>
+        case _ => println("This is not a valid option. Please select again.")
+      }
+    }
+  }
+
+  private val menu =
 }
