7e105f5704e9fdf30ed5b2effbee415a366e4d55
Raja Shekar <irajashekarb@gmail.com>
2019-10-07 16:17:28 +0530

    Update Student.scala

--- Example programs/src/programs/Student.scala
+++ Example programs/src/programs/Student.scala
@@ -11,7 +11,7 @@ class Student (
                         else (quizzes.sum - quizzes.min).toDouble / (quizzes.length - 1)
   def assignmentAverage: Double = if(assignments.isEmpty) 0.0
                                   else assignments.sum.toDouble / assignments.length
-  def testAverage: Double = if (tests.isEmpty) 0.0 
+  def testAverage: Double = if (tests.isEmpty) 0.0
                             else tests.sum.toDouble / tests.length
   def average: Double = quizAvg * 0.1 + assignmentAverage * 0.5 + testAverage * 0.4
 }
