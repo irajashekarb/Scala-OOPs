010bc3d089dcc95a34206158f9494f2bd4495138
Raja Shekar <irajashekarb@gmail.com>
2019-10-07 16:27:41 +0530

    Update bank.package
    This package contains all bank implementation files.

---
+++ Example programs/src/bank/Bank.scala
@@ -0,0 +1,15 @@
+package bank
+
+class Bank (private val customer: List[Customer], private val account: List[Account]) {
+//  def addCustomer(fname: String, lname: String): Unit = {
+//
+//  }
+//
+//  def openAccount(c: Customer): Account = {
+//
+//  }
+//
+//  def closeAccount(a: Account): Boolean = {
+//
+//  }
+}
