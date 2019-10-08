38918ecfecf9bc7c3e700bec97cde78583a61475
Raja Shekar <irajashekarb@gmail.com>
2019-10-08 15:01:25 +0530

    update table package

--- Example programs/src/table/tableobj.scala
+++ Example programs/src/table/tableobj.scala
@@ -2,11 +2,14 @@ package table
 
 object tableobj {
   def main(args: Array[String]): Unit = {
-    try {
-      val t1 = new table
-      t1.getNum
-    } catch {
-      case e: NumberFormatException => val t1 = new table; t1.getNum
+    def tableGetter: AnyVal = {
+        try {
+          val t1 = new table
+          t1.getNum
+        } catch {
+          case e: NumberFormatException => tableGetter;
+        }
     }
+    tableGetter
   }
 }
